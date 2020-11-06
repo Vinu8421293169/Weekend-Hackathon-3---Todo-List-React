404682b89d6927b63e6d4b0e0fd2bd5e065d4ac4
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-06 10:58:53 +0530

    commit ok

--- src/components/taskView.jsx
+++ src/components/taskView.jsx
@@ -8,20 +8,18 @@ export default function TaskView({
 }) {
   return (
     <>
-      <li className="list">
-        <div>{item.text}</div>
-        <button className="edit" onClick={() => handleEdit(item.id)}>
-          Edit
+      <li className="list">{item.text}</li>
+      <button className="edit" onClick={() => handleEdit(item.id)}>
+        Edit
+      </button>
+      <button className="delete" onClick={() => handleDelete(item.id)}>
+        Delete
+      </button>
+      {item.edit && (
+        <button className="saveTask" onClick={() => handleSave(item.id)}>
+          Save
         </button>
-        <button className="delete" onClick={() => handleDelete(item.id)}>
-          Delete
-        </button>
-        {item.edit && (
-          <button className="saveTask" onClick={() => handleSave(item.id)}>
-            Save
-          </button>
-        )}
-      </li>
+      )}
     </>
   );
 }
